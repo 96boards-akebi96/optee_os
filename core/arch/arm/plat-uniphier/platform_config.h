51eda1f5dea1e29e19c5f36d10b2a7b02a4ecceb
Tetsuya Yoshizaki <yoshizaki.tetsuya@socionext.com>
2018-10-01 16:05:38 +0900

    core: add initial UniPhier platform
    Add support for Socionext UniPhier SoCs.
    
    Changes from original code:
    - remove uniphier_uart and use serial8250_uart
    - replace license notice with SPDX-License-Identifier
    - fix some expressions according to Linux style
    - update config.h for the latest optee_os
    
    Some features are not reflected:
    - MAINTAINERS
    - README.md, because we can't introduce the series yet
    
    Signed-off-by: Tetsuya Yoshizaki <yoshizaki.tetsuya@socionext.com>
    Signed-off-by: Kunihiko Hayashi <hayashi.kunihiko@socionext.com>

---
+++ core/arch/arm/plat-uniphier/platform_config.h
@@ -0,0 +1,66 @@
+/* SPDX-License-Identifier: BSD-2-Clause */
+/*
+ * Copyright (c) 2015, Linaro Limited
+ * Copyright (c) 2017, Socionext Inc.
+ */
+
+#ifndef PLATFORM_CONFIG_H
+#define PLATFORM_CONFIG_H
+
+/* Make stacks aligned to data cache line length */
+#define STACK_ALIGNMENT		64
+
+/* GIC */
+#define GIC_BASE		0x5FE00000
+#define GICD_OFFSET		0
+#define GICC_OFFSET		0x80000
+
+/* UART */
+#define UART_CH			0
+#define UART_BASE		0x54006800
+#define CONSOLE_UART_BASE	(UART_BASE + 0x100 * UART_CH)
+#define CONSOLE_BAUDRATE	115200
+#define CONSOLE_UART_CLK_IN_HZ	58820000
+
+/*
+ *  0xXXXX_XXXX                               -           -
+ *    Linux kernel                            |           |
+ *  0x9600_0000                               | DRAM#0-#x | Normal memory
+ *    Reserved area for Linux drivers         |           |
+ *  0x8200_0000                               -           -
+ *    TA RAM: 14MiB                           |           |
+ *  0x8118_0000                               | TZDRAM    |
+ *    TEE RAM: 1 MiB (CFG_TEE_RAM_VA_SIZE)    |           | Secure memory
+ *  0x8108_0000 [TZDRAM_BASE, BL32_LOAD_ADDR] -           |
+ *    BL31 runtime (defined in ATF code)      |           |
+ *  0x8100_0000                               |           -
+ *    TEE-REE Shared memory                   | DRAM#0    |
+ *  0x80E0_0000                               |           | Normal memory
+ *    Reserved area for ucode                 |           |
+ *  0x8000_0000 [DRAM0_BASE]                  -           -
+ */
+
+#define DRAM0_BASE		0x80000000
+#define DRAM0_SIZE		0xC0000000
+
+#define TZDRAM_BASE		0x81080000
+#define TZDRAM_SIZE		(14 * 1024 * 1024)
+
+#define TEE_SHMEM_START		0x80E00000
+#define TEE_SHMEM_SIZE		(2 * 1024 * 1024)
+
+#define CFG_TEE_CORE_NB_CORE	4
+
+#define TEE_RAM_VA_SIZE		(1024 * 1024)
+
+#define TEE_LOAD_ADDR		TZDRAM_BASE
+
+#define TEE_RAM_PH_SIZE		TEE_RAM_VA_SIZE
+#define TEE_RAM_START		TZDRAM_BASE
+#define TA_RAM_START		ROUNDUP((TZDRAM_BASE + TEE_RAM_VA_SIZE), \
+					CORE_MMU_DEVICE_SIZE)
+
+#define TA_RAM_SIZE		ROUNDDOWN((TZDRAM_SIZE - TEE_RAM_VA_SIZE),\
+					  CORE_MMU_DEVICE_SIZE)
+
+#endif /* PLATFORM_CONFIG_H */
